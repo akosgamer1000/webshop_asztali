d9a776ed6b45081a1a9eaf8cfbe8d21a06ba51ea
Petrik Diak <diak@petrik.hu>
2025-01-17 16:00:55 +0100

    terter

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -2,7 +2,12 @@ import {app,BrowserWindow} from 'electron'
 import path from 'path'
 
 app.on('ready',()=>{
-    let mainWindow = new BrowserWindow({ })
+    let mainWindow = new BrowserWindow({ 
+        width: 1200, 
+        height: 800, 
+      
+
+    })
     mainWindow.loadFile(path.join( app.getAppPath() + '/dist-react/index.html'))
 
 
