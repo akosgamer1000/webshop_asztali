174b92c8d6003b61de813dd25c74648025635f63
akosgamer1000 <nemet.akos123@gmail.com>
2025-03-20 18:20:55 +0100

    l

--- src/ui/companents/logincontent.tsx
+++ src/ui/companents/logincontent.tsx
@@ -83,8 +83,8 @@ const LoginContent: React.FC = () => {
 
  
   return (
-    <div className="h-full flex items-center justify-center bg-gray-100">
-      <div className="max-w-md w-full space-y-8 p-8 bg-white rounded-lg shadow-lg">
+    <div className="flex-1 overflow-x-auto mt-5 flex items-center justify-center">
+      <div className="max-w-md w-full space-y-8 p-8 bg-white rounded-lg shadow-lg m-4">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Sign in to your account
